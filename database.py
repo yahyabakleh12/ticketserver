ff08af4fa9335f925319003c1819f8d3dd470688
Yahya Bakleh <123378314+yahyabakleh12@users.noreply.github.com>
2025-07-23 10:29:17 +0400

    Refactor submit endpoint to use BackgroundTasks and tune pool

--- database.py
+++ database.py
@@ -4,6 +4,11 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "mysql+pymysql://admin1:Dev!12345@localhost:3306/ticket_management"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_size=10,
+    max_overflow=20,
+    pool_timeout=30
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
